5cecb89b719749de61d9b84422738b221a34cbf4
edward <lxfg_cn@163.com>
2023-09-01 23:31:18 +0800

    init the first version

---
+++ src/main/java/com/chef/assist/config/RESTAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.chef.assist.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class RESTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        log.info("RESTAuthenticationEntryPoint msg={}, cause={}",authException.getMessage(), authException.getLocalizedMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
